082983647615e2bc45e7d40cfa9aa7833ad7214b
jalsson <nilsson.niklas.97@gmail.com>
2022-03-08 12:26:45 +0200

    new update script

---
+++ LongvinterUpdater.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo checking for updates
+
+UpdateServer () {
+        systemctl is-active --quiet longvinter && echo "Server is running!"
+        sudo systemctl stop longvinter && echo "Server is now closed, now getting latest version..."
+        git restore .
+        sleep 1
+        git pull "https://github.com/Uuvana-Studios/longvinter-experimental.git" main
+        sleep 1
+        echo "Starting server..."
+        sudo chmod -R ugo+rwx /home/steam/longvinter-linux-server/
+        sudo systemctl start longvinter && echo "Server is now running!"
+}
+
+cd ~/longvinter-linux-server
+
+git fetch
+
+if git merge-base --is-ancestor origin/main main ; then
+        echo "Nothing to update"
+else
+        echo "Change found, updating server.."
+        UpdateServer
+fi
